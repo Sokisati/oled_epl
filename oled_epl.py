b1e9b270d3b2eb1fe9a0b83fff540da001ea8d5b
Sokisati <yagizalpersoy@gmail.com>
2024-07-24 13:50:26 +0300

    image path

--- oled_epl.py
+++ oled_epl.py
@@ -1,35 +1,25 @@
 import Adafruit_SSD1306
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 import time
 
+# Initialize the library (size of the display)
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=None, i2c_address=0x3C)
 
+# Clear display
 disp.begin()
 disp.clear()
 disp.display()
 
-# Create image to draw on
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
-draw = ImageDraw.Draw(image)
+# Load image
+image = Image.open('/home/pi/oled_epl/logo.bmp').convert('1')
 
-# Draw a black filled box to clear the image
-draw.rectangle((0, 0, width, height), outline=0, fill=0)
-
-font = ImageFont.load_default()
-
-# Write some text
-text = "Hello, World!"
-draw.text((0, 0), text, font=font, fill=255)
 
 # Display image
 disp.image(image)
 disp.display()
 
-time.sleep(2)
+time.sleep(10)
 
 # Clear the display
 disp.clear()
 disp.display()
-
